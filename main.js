4abcf91bbb35daac3612b3d9dafb9e4e1b81b9ed
Crystal Young <crystal.young@codeimmersives.com>
2020-07-16 14:46:43 -0400

    add

---
+++ main.js
@@ -0,0 +1,39 @@
+const fs = require('fs', '\n');
+const { constants } = require('fs');
+const { Console } = require('fs');
+
+const userInput = process.argv[2]
+const getDate = date
+const getTotalCases = total[5]
+const searchDate = date[3]
+const getRows = getRows
+
+
+const searchDate = function (lookup) {
+    let(i = 0; lookup.length; i++) {
+        if (userInput === searchDate) {
+            return searchDate
+        } else {
+            return error('type date in YYYY-MM-DD format')
+        } throw ('Something failed. Please try again.')
+
+    }
+}
+
+const inquiry2 = function (newNumber, totalCases) {
+    for
+        (const items of newNumber) {
+        if (userInput === searchDate || userInput === casesTotal) {
+            return date[3], total[5]
+        } let then go ahead and get the sum ({ date[3]} + {
+            total[5])
+            else if(userInput === casesByDate) {
+            return newNumber[6]
+        } else {
+            return ('Something failed. Please try again.')
+        }
+
+        const inquiry1 = function (date) {
+            return date[3]
+        }
+
