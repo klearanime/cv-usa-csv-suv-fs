c509475ba4a50f181b1f4eb2f8584e706525ddb5
Crystal K Young <crystalyoung@Crystals-MacBook-Pro.local>
2020-07-17 13:16:29 -0400

    add

--- main.js
+++ main.js
@@ -1,39 +1,34 @@
-const fs = require('fs', '\n');
-const { constants } = require('fs');
-const { Console } = require('fs');
+const fs = require('fs');
+const cvData = fs.readFileSync('./owid-covid-usa-data.csv', {encoding:'utf-8'});
+const rows = require('./get-rows.js');
+const newCases = require('./get-new-cases-by-day.js');
+const total = require('./get-total-cases-by-day.js');
+const getNewCasesByDay = require('./get-new-cases-by-day.js');
 
-const userInput = process.argv[2]
-const getDate = date
-const getTotalCases = total[5]
-const searchDate = date[3]
-const getRows = getRows
+const userInput = process.argv[2];
+const searchDate = process.argv[3];
+const total = process.argv[4];
 
-
-const searchDate = function (lookup) {
-    let(i = 0; lookup.length; i++) {
-        if (userInput === searchDate) {
-            return searchDate
+//const searchDate = function (lookup) {
+//    let(i = 0; lookup.length; i++) {
+        if (userInput === 'total') {
+            console.log(newCases)
+        
+    (otherwise)   else {
+        
+        throw error('Something failed. Please try again.')
+            }
+        
+//const inquiry2 = function (newNumber, totalCases) {
+//    for (const items of newNumber) {
+} 
+        if (searchDate === 'cases for today') {
+console.log('Cases for ' + (newCases(searchDate, cvData)) 
+        else if (userInput === searchDate || userInput === 'total') {
+            console.log(newCases[3], total[5])
+     //    then go ahead and get the sum 
+        }   else if (userInput === getNewCasesByDay) {
+            console.log(total[6])
         } else {
-            return error('type date in YYYY-MM-DD format')
-        } throw ('Something failed. Please try again.')
-
-    }
-}
-
-const inquiry2 = function (newNumber, totalCases) {
-    for
-        (const items of newNumber) {
-        if (userInput === searchDate || userInput === casesTotal) {
-            return date[3], total[5]
-        } let then go ahead and get the sum ({ date[3]} + {
-            total[5])
-            else if(userInput === casesByDate) {
-            return newNumber[6]
-        } else {
-            return ('Something failed. Please try again.')
+            throw error('type date in YYYY-MM-DD format')
         }
-
-        const inquiry1 = function (date) {
-            return date[3]
-        }
-
